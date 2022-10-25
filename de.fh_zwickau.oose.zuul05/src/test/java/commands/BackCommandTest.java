20859f62957b785a71dd53a4b283458cc32962ad
Ammar Chaleh <Ammar.Chaleh.lkv@fh-zwickau.de>
2022-10-25 23:07:17 +0200

    Installation BackCommandTest

---
+++ de.fh_zwickau.oose.zuul05/src/test/java/commands/BackCommandTest.java
@@ -0,0 +1,14 @@
+package commands;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BackCommandTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
