00401316a7580984418819ec5284d48dc8217dce
aymen <aymen.tekari.lku@fh-zwickau.de>
2022-10-26 12:58:15 +0200

    add additional JUnit Test class for the command "gehe" write the description of each command

--- de.fh_zwickau.oose.zuul05/src/main/java/de/fh_zwickau/oose/zuul05/Game/Game.java
+++ de.fh_zwickau.oose.zuul05/src/main/java/de/fh_zwickau/oose/zuul05/Game/Game.java
@@ -50,14 +50,14 @@ class Game
      */
     private void createRooms()
     {
-        Room draussen, hoersaal, mensa, computerraum, adminraum;
+        Room schlaffraum, obstraum, essenraum, getraenkraum, dach;
       
         // Räume erzeugen:
-        draussen = new Room("vor dem Haupteingang der Hochschule");
-        hoersaal = new Room("in einem Hörsaal");
-        mensa = new Room("in der Mensa");
-        computerraum = new Room("in einem Computerraum");
-        adminraum = new Room("im Raum des Computer-Admins");
+        schlaffraum = new Room("im Ruheraum");
+        obstraum = new Room("iPlatz der Kategorie Obst");
+        essenraum = new Room("Platz der Kategorie Essen");
+        getraenkraum = new Room("Platz der Kategorie Getraenke");
+        schiffsdach = new Room("in der Schiff Oberfläche");
         
         // Ausgänge aufbauen:
         draussen.setExit("osten", hoersaal);
@@ -74,7 +74,7 @@ class Game
         adminraum.setExit("westen", computerraum);
         
         // Der Spieler startet das Spiel draußen vor der Hochschule:
-        player.setCurrentRoom(draussen);
+        player.setCurrentRoom(schlaffraum);
     }
 
 
@@ -103,8 +103,8 @@ class Game
     private void printWelcome()
     {
         System.out.println();
-        System.out.println("Willkommen in der Welt von Zuul!");
-        System.out.println("Die Welt von Zuul ist ein neues, unglaublich langweiliges Adventure-Spiel.");
+        System.out.println("Willkommen beim Keep Breathing !");
+        System.out.println("das Spiel des Überlebens und des Lebens neuer Abenteuer.");
         System.out.println("Gib 'hilfe' ein, um Hilfe zu bekommen.");
         System.out.println();
         System.out.println(player.getCurrentRoom().getLongDescription());
