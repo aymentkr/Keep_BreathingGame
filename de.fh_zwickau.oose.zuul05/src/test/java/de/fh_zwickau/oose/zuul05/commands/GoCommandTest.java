173a17c7591e635453c8d83bbe3cb2c950812295
Ammar Chaleh <Ammar.Chaleh.lkv@fh-zwickau.de>
2022-10-31 20:56:10 +0100

    in GoCommandTest Class void testStartRoom getestet

--- de.fh_zwickau.oose.zuul05/src/test/java/de/fh_zwickau/oose/zuul05/commands/GoCommandTest.java
+++ de.fh_zwickau.oose.zuul05/src/test/java/de/fh_zwickau/oose/zuul05/commands/GoCommandTest.java
@@ -1,7 +1,26 @@
 package de.fh_zwickau.oose.zuul05.commands;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import de.fh_zwickau.oose.zuul05.Game;
 
 class GoCommandTest {
+
+    public Game game;
+    public GoCommand goCommand;
+
+    @BeforeEach
+    void init() {
+        this.game = new Game();
+        this.goCommand = new GoCommand();
+
+    }
+    @Test
+    void testStartRoom() {
+        assertEquals("im Ruheraum", game.getPlayer().getCurrentRoom().getShortDescription());
+
+    }
 
 }
