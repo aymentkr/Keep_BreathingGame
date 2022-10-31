d7e110da09f7e9d4cba6c9021c99ebf99e64b832
Aljarrah Mohamad Ghaith <Mohamad.Aljarrah.lch@fh-zwickau.de>
2022-10-31 20:10:04 +0100

    Init zum HelpCommandTest  hinzugefügt

--- de.fh_zwickau.oose.zuul05/src/test/java/de/fh_zwickau/oose/zuul05/commands/HelpCommandTest.java
+++ de.fh_zwickau.oose.zuul05/src/test/java/de/fh_zwickau/oose/zuul05/commands/HelpCommandTest.java
@@ -2,6 +2,22 @@ package de.fh_zwickau.oose.zuul05.commands;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class HelpCommandTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-}
+import de.fh_zwickau.oose.zuul05.Game;
+
+class HelpCommandTest {
+    public Game game;
+    public HelpCommand helpCommand;
+    public CommandWords commandWords;
+
+    @BeforeEach
+    void init() {
+        this.game=new Game();
+        this.commandWords=new CommandWords();
+        this.helpCommand=new  HelpCommand(commandWords);
+
+
+
+    }}
