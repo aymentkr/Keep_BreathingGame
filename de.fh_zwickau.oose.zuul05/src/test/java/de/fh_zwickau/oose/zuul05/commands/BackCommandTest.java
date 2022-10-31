8c1d1b96fa92e639ecc9e0c015cbd1cf8f340cd0
Aljarrah Mohamad Ghaith <Mohamad.Aljarrah.lch@fh-zwickau.de>
2022-10-31 19:59:23 +0100

    Init methode hinzufügt

---
+++ de.fh_zwickau.oose.zuul05/src/test/java/de/fh_zwickau/oose/zuul05/commands/BackCommandTest.java
@@ -0,0 +1,23 @@
+package de.fh_zwickau.oose.zuul05.commands;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import de.fh_zwickau.oose.zuul05.Game;
+
+class HelpCommandTest2 {
+    public Game game;
+    public HelpCommand helpCommand;
+    public CommandWords commandWords;
+
+    @BeforeEach
+    void init() {
+        this.game=new Game();
+        this.commandWords=new CommandWords();
+        this.helpCommand=new  HelpCommand(commandWords);
+
+
+
+    }}
