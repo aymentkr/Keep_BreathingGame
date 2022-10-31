125656460fed7745de360458315ab098607db176
Ammar Chaleh <Ammar.Chaleh.lkv@fh-zwickau.de>
2022-10-31 20:59:37 +0100

    in GoCommandTest Class testGoCommandGetDescription-testGoCommandSetSecondWordAndEcecute getestet

--- de.fh_zwickau.oose.zuul05/src/test/java/de/fh_zwickau/oose/zuul05/commands/GoCommandTest.java
+++ de.fh_zwickau.oose.zuul05/src/test/java/de/fh_zwickau/oose/zuul05/commands/GoCommandTest.java
@@ -22,5 +22,27 @@ class GoCommandTest {
         assertEquals("im Ruheraum", game.getPlayer().getCurrentRoom().getShortDescription());
 
     }
+    @Test
+    void testGoCommandGetDescription() {
+        assertEquals("""
+			    Beweg in einen anderen Raum
+			    Es gibt viele Möglichkeiten, sich durch Tippen von Raum zu Raum zu bewegen
+			    wie folgt: - gehe links
+			               - gehe recht
+			               - gehe unten
+			               - gehe oben
+			    """, goCommand.getDescription());
+
+    }
+
+    @Test
+    void testGoCommandSetSecondWordAndEcecute() {
+        goCommand.setSecondWord("unten");
+        goCommand.execute(game.getPlayer());
+
+        assertEquals("Platz der Kategorie Essen", game.getPlayer().getCurrentRoom().getShortDescription());
+
+    }
+
 
 }
