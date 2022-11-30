f0e7372f3eed24d0786e8147d6147cdcc044a3f4
Aljarrah Mohamad Ghaith <Mohamad.Aljarrah.lch@fh-zwickau.de>
2022-11-30 20:41:18 +0100

    BackCommandTest.java besser implementieren

--- de.fh_zwickau.oose.zuul05/src/test/java/de/fh_zwickau/oose/zuul05/model/commands/BackCommandTest.java
+++ de.fh_zwickau.oose.zuul05/src/test/java/de/fh_zwickau/oose/zuul05/model/commands/BackCommandTest.java
@@ -13,22 +13,25 @@ import static org.testng.AssertJUnit.assertEquals;
 
 class BackCommandTest {
 
-    public static Player player;
-    public static BackCommand backCommand;
-    public static GoCommand goCommand ;
+    public  Player player;
+    public  BackCommand backCommand;
+    public  GoCommand goCommand ;
 
     public static Room schlaffraum;
     public static Room essenraum;
     public static Room schiffsdach;
     public static Room getraenkraum;
+    public static Room obstraum;
 
     @BeforeAll
     static void  setStruktur(){
         //creat rooms
+        obstraum=new Room("in obstRaum");
         schlaffraum = new Room("im Ruheraum");
         essenraum = new Room("Platz der Kategorie Essen");
         essenraum.setGeschlossen(true);
         schiffsdach = new Room("in der Schiff Oberfläche");
+        getraenkraum=new Room("in Getraenk Raum");
         //add exits for rooms
         schlaffraum.setExit("unten", essenraum);
         schlaffraum.setExit("links", schiffsdach);
@@ -37,7 +40,13 @@ class BackCommandTest {
         essenraum.setExit("oben", schlaffraum);
 
         schiffsdach.setExit("recht",schlaffraum);
+        schiffsdach.setExit("unten",obstraum);
 
+        obstraum.setExit("oben",schiffsdach);
+        obstraum.setExit("recht",getraenkraum);
+
+        getraenkraum.setExit("recht",essenraum);
+        getraenkraum.setExit("links",obstraum);
 
     }
     @BeforeEach
@@ -70,6 +79,33 @@ class BackCommandTest {
 
         backCommand.execute(player);
         assertEquals("in der Schiff Oberfläche", player.getCurrentRoom().getShortDescription());
+    }
+
+    @Test
+    void testBackCommandWithLastRoomInBoatStruktur(){
+        player.setCurrentRoom(schlaffraum);
+        goCommand.setSecondWord("links");
+        goCommand.execute(player);
+        goCommand.setSecondWord("unten");
+        goCommand.execute(player);
+        goCommand.setSecondWord("recht");
+        goCommand.execute(player);
+        essenraum.setGeschlossen(false);
+        goCommand.setSecondWord("recht");
+        goCommand.execute(player);
+
+      assertEquals("Platz der Kategorie Essen", player.getCurrentRoom().getShortDescription());
+
+        backCommand.execute(player);
+        assertEquals("in Getraenk Raum", player.getCurrentRoom().getShortDescription());
+        backCommand.execute(player);
+        assertEquals("in obstRaum", player.getCurrentRoom().getShortDescription());
+        backCommand.execute(player);
+        assertEquals("in der Schiff Oberfläche", player.getCurrentRoom().getShortDescription());
+        backCommand.execute(player);
+        assertEquals("im Ruheraum", player.getCurrentRoom().getShortDescription());
+
+
 
 
     }
